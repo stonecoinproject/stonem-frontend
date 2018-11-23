02282134da88af9fca7ed72617d6c7e493b8cfdb
creatrixity <creatrixity@gmail.com>
2018-11-23 21:23:15 +0100

    restage repo files

--- src/components/UI/CapsText.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import styledComponents from 'styled-components';
-import { Text } from 'rebass';
-
-const capsText = styledComponents(Text)`
-  text-transform: uppercase
-`;
-
-export default capsText;
